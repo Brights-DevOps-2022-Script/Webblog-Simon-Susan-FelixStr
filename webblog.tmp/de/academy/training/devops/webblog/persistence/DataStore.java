89f41a726a7dccde1a31c03d9068d49a2a85755e
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-18 11:55:30 +0100

    datastore file created

--- webblog.tmp/de/academy/training/devops/webblog/persistence/DataStore.java
+++ webblog.tmp/de/academy/training/devops/webblog/persistence/DataStore.java
@@ -1,5 +1,24 @@
 package de.academy.training.devops.webblog.persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+import de.academy.training.devops.webblog.backend.Post;
+
 public class DataStore {
-    
+
+    private Map<Integer, Post> postMap;
+    private int nextPostId = 1;
+
+    public DataStore() {
+        postMap = new HashMap<>();
+    }
+
+    public void addPost(Post post) {
+        postMap.put(nextPostId, post);
+        nextPostId++;
+    }
+
+    public Post getPost(int id) {
+        return postMap.get(id);
+    }
 }
