d0373ca7151461028ecf84d3ef1840a025e3a652
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-18 16:02:23 +0100

    234g

--- webblog.tmp/de/academy/training/devops/webblog/persistence/DataStore.java
+++ webblog.tmp/de/academy/training/devops/webblog/persistence/DataStore.java
@@ -31,13 +31,25 @@ public class DataStore {
         post.setId(nextPostId);
         postMap.put(nextPostId, post);
         nextPostId++;
-        savePostsToCsv("posts.csv");
         System.out.println("--------------------Your post has been created and!--------------------");
     }
 
     public void addComment(Comment comment) {
         commentMap.put(nextCommentId, comment);
         nextCommentId++;
+    }
+
+    public void showAllPosts() {
+        for (int i = 1; i < nextPostId; i++) {
+            Post post = postMap.get(i);
+            System.out.println("ID: " + i);
+            System.out.println("Title: " + post.getTitle());
+            System.out.println("Text: " + post.getText());
+            System.out.println("Author: " + post.getAuthor());
+            System.out.println("Comments: " + post.getComments());
+            System.out.println("Date: " + post.getFormattedDateTime());
+            System.out.println("--------------------------------------------------");
+        }
     }
 
     public Post getPost(int id) {
@@ -54,32 +66,5 @@ public class DataStore {
 
     public Comment getCommentId() {
         return commentMap.get(nextCommentId);
-    }
-
-    public void savePostsToCsv(String filePath) {
-        File file = new File(filePath);
-        try (PrintWriter writer = new PrintWriter(file)) {
-            for (Map.Entry<Integer, Post> entry : postMap.entrySet()) {
-                Post post = entry.getValue();
-                String line = nextPostId + "," + post.getTitle() + "," + post.getText() + "," + post.getAuthor() + ","
-                        + post.getFormattedDateTime();
-                writer.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void showAllPosts() {
-        for (int i = 1; i < nextPostId; i++) {
-            Post post = postMap.get(i);
-            System.out.println("ID: " + i);
-            System.out.println("Title: " + post.getTitle());
-            System.out.println("Text: " + post.getText());
-            System.out.println("Author: " + post.getAuthor());
-            System.out.println("Comments: " + post.getComments());
-            System.out.println("Date: " + post.getFormattedDateTime());
-            System.out.println("--------------------------------------------------");
-        }
     }
 }
