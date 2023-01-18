dcf7c3b911392622ec738d982530628d51ff25b4
Felix Strauss <felix.strauss.arbeit@gmail.com>
2023-01-18 12:00:44 +0100

    Update

--- webblog.tmp/de/academy/training/devops/webblog/backend/Comment.java
+++ webblog.tmp/de/academy/training/devops/webblog/backend/Comment.java
@@ -1,18 +1,30 @@
 package de.academy.training.devops.webblog.backend;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Comment {
- 
+
     private String text;
-    private String author;
-    
-        public Comment(String text, String author) {
-            this.text = text;
-            this.author = author;
-            
+    private LocalDateTime dateTime;
+    private DateTimeFormatter formatter;
+
+    public Comment(String text, String author) {
+        this.text = text;
+        this.dateTime = LocalDateTime.now();
+        this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
     }
 
     public String getText() {
         return text;
     }
+    public LocalDateTime getLocalDateTime() {
+        return dateTime;
+    }
+
+    public String getFormattedDateTime() {
+        return dateTime.format(formatter);
+    }
+
 }
--- webblog.tmp/de/academy/training/devops/webblog/backend/Post.java
+++ webblog.tmp/de/academy/training/devops/webblog/backend/Post.java
@@ -1,22 +1,24 @@
 package de.academy.training.devops.webblog.backend;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-
-
-
-public class Post{
+public class Post {
 
     private String title;
     private String text;
     private String author;
     private ArrayList<Comment> comment;
-
-    
+    private LocalDateTime dateTime;
+    private DateTimeFormatter formatter;
 
     public Post(String title, String text, String author) {
         this.title = title;
         this.text = text;
         this.author = author;
+        this.dateTime = LocalDateTime.now();
+        this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         this.comment = new ArrayList<Comment>();
 
     }
@@ -28,16 +30,28 @@ public class Post{
     public String getText() {
         return text;
     }
-    
+
     public String getAuthor() {
         return author;
     }
+
     public ArrayList<Comment> getComments() {
         return comment;
 
+    }
 
-        }
+    public LocalDateTime getLocalDateTime() {
+        return dateTime;
+    }
+
+    public String getFormattedDateTime() {
+        return dateTime.format(formatter);
+    }
+
     private ArrayList<Comment> comments = new ArrayList<Comment>();
+
     public void addComment(Comment comment) {
         comments.add(comment);
-}}
+    }
+
+}
