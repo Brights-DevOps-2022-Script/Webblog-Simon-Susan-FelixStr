5ad947defda58f10d91b638c175860232c667a04
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-19 14:47:08 +0100

    user class created

---
+++ webblog.tmp/de/academy/training/devops/webblog/backend/User.java
@@ -0,0 +1,11 @@
+package de.academy.training.devops.webblog.backend;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String admin;
+    private String adminpassword;
+
+    
+}
