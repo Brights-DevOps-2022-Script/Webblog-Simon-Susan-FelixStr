c65c8ac373bd93b6001172501b5d6c1ee2d79515
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-19 11:54:17 +0100

    test update

--- webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
+++ webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 import de.academy.training.devops.webblog.backend.Post;
-import de.academy.training.devops.webblog.persistence.DataStore;
 
 public class DatastoreTest {
     
