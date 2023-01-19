0d337be1f044326962ba206fe9c9a0e35fdf73f9
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-19 11:54:53 +0100

    update test

--- webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
+++ webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
@@ -6,21 +6,21 @@ import org.junit.Test;
 import de.academy.training.devops.webblog.backend.Post;
 
 public class DatastoreTest {
-    
-    // test vorbereiten
+
     @Test
     public void testcreatePost() {
+        // test vorbereiten
         DataStore dataStore = new DataStore();
 
+        // test logik
         dataStore.createPost("test title", "test text", "test author");
 
         Post post = dataStore.getPost(1);
 
-        //test erwartung
+        // test erwartung
         assertEquals("test title", post.getTitle());
         assertEquals("test text", post.getText());
         assertEquals("test author", post.getAuthor());
     }
-
 
 }
