84671ba43373b9312e9ff367d5f25c1400c028dc
simonoscr <117449098+simonoscr@users.noreply.github.com>
2023-01-19 11:53:58 +0100

    DataStoreTest createPost test created

--- webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
+++ webblog.tmp/de/academy/training/devops/webblog/persistence/DatastoreTest.java
@@ -1,5 +1,27 @@
 package de.academy.training.devops.webblog.persistence;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import de.academy.training.devops.webblog.backend.Post;
+import de.academy.training.devops.webblog.persistence.DataStore;
+
 public class DatastoreTest {
     
+    // test vorbereiten
+    @Test
+    public void testcreatePost() {
+        DataStore dataStore = new DataStore();
+
+        dataStore.createPost("test title", "test text", "test author");
+
+        Post post = dataStore.getPost(1);
+
+        //test erwartung
+        assertEquals("test title", post.getTitle());
+        assertEquals("test text", post.getText());
+        assertEquals("test author", post.getAuthor());
+    }
+
+
 }
